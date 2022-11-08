28a57c3a1d1a620bc10b3a9705f2bd7460f3c41c
nhat_minh <hnnminh2411@gmail.com>
2022-11-08 08:13:21 +0700

    #18 - Create Order Screen

---
+++ pages/api/orders/[id].ts
@@ -0,0 +1,19 @@
+import {getSession} from "next-auth/react";
+import Order from "../../../models/Order";
+import db from "../../../utils/db";
+import {NextApiRequest, NextApiResponse} from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({req});
+  if (!session) {
+    return res.status(401).send("signin required");
+  }
+
+  await db.connect();
+
+  const order = await Order.findById(req.query.id);
+  await db.disconnect();
+  res.send(order);
+};
+
+export default handler;
